8ba199cfe33854963c2aa9e8c5c6403eb473e4bc
Ernest Petrovich <ernest.petrovic25@gmail.com>
2025-12-03 10:25:36 +0200

    Dataset processing

--- src/stream_mining/__init__.py
+++ src/stream_mining/__init__.py
@@ -3,6 +3,7 @@ __version__ = "0.1.0"
 from stream_mining.io_xes import Event, Log, Trace, read_xes, to_xes
 from stream_mining.sliding_window import SlidingWindowCount, SlidingWindowTime
 from stream_mining.decay_list import DecayList
+from stream_mining.prepare_dataset import build_directly_follows, prepare_for_mining
 
 __all__ = [
     "Event",
@@ -13,4 +14,6 @@ __all__ = [
     "SlidingWindowCount",
     "SlidingWindowTime",
     "DecayList",
+    "build_directly_follows",
+    "prepare_for_mining",
 ]
